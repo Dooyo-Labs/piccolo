14ed69751b1ec753f7de2e8f6165f5666c50d2c0
Aeledfyr <aeledfyr@gmail.com>
2024-06-30 15:45:45 -0500

    Implement load (for text chunks only)

--- src/stdlib/mod.rs
+++ src/stdlib/mod.rs
@@ -2,11 +2,12 @@ mod base;
 mod coroutine;
 mod debug;
 mod io;
+mod load;
 mod math;
 mod string;
 mod table;
 
 pub use self::{
-    base::load_base, coroutine::load_coroutine, debug::load_debug, io::load_io, math::load_math,
+    base::load_base, coroutine::load_coroutine, debug::load_debug, io::load_io, load::load_load_text, math::load_math,
     string::load_string, table::load_table,
 };
